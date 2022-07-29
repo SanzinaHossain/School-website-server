37364fcc70db782196afc8470bbe2c5d6c9b5297
SanzinaHossain <sanzina67hossain@gmail.com>
2022-07-29 15:24:05 +0600

    package

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const cors =require('cors');
+require('dotenv').config();
+const app = express()
+const port =process.env.PORT || 5000
+//middle ware 
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('School Website')
+})
+
+app.listen(port, () => {
+  console.log(`School app listening on port ${port}`)
+})
